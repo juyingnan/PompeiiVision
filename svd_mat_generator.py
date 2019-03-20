c7f2aff06d2e39ffa98cfe1e795d5993bbace6f9
Yingnan Ju <bunny_gg@hotmail.com>
2019-03-19 22:52:21 -0400

    add anti-aliasing and better refactor

--- svd_mat_generator.py
+++ svd_mat_generator.py
@@ -31,7 +31,7 @@ def read_img_random(path, total_count, as_gray=False):
             #     _img = transform.resize(_img, (w, h))
             #     imgs.append(_img)
             #     labels.append(idx)
-            img = transform.resize(img, (w, h))
+            img = transform.resize(img, (w, h), anti_aliasing=True)
             imgs.append(img)
             labels.append(idx)
             file_names.append(im.split('\\')[-1])
@@ -44,22 +44,60 @@ def read_img_random(path, total_count, as_gray=False):
         indexes, np.int32)
 
 
-if __name__ == '__main__':
-    w = 20
-    h = 20
-    c = 3
-    train_image_count = 10000
-    train_path = r'D:\Projects\pompeii\test_20180919\svd_test/'
-    train_data, train_label, train_file_paths, train_indexes = read_img_random(train_path, train_image_count,
-                                                                               as_gray=True)
-    np.seterr(all='ignore')
-
-    # d2_train_data = feature_test.get_shape_index_features(train_data, size=10)
-    # d2_train_data = k_means_test.get_features(train_data, whole_image_sample=True, frame_sample=False,
-    #                                         global_color=True, composition=True, segment_color=True, sift=True)
-    d2_train_data = k_means_test.get_raw_pixel_features(train_data)
-    sio.savemat('mat/raw_' + str(w) + '.mat',
+def save_raw_mat(raw_data, file_name):
+    d2_train_data = k_means_test.get_raw_pixel_features(raw_data)
+    print('raw mat size: ', d2_train_data.shape)
+    sio.savemat(file_name,
                 mdict={'feature_matrix': d2_train_data,
                        'label': train_label,
                        'file_name': train_file_paths,
                        'index': train_indexes})
+
+
+def save_shape_index_mat(raw_data, size, file_name):
+    d2_train_data = feature_test.get_shape_index_features(raw_data, size=size)
+    print('shape index mat size: ', d2_train_data.shape)
+    sio.savemat(file_name,
+                mdict={'feature_matrix': d2_train_data,
+                       'label': train_label,
+                       'file_name': train_file_paths,
+                       'index': train_indexes})
+
+
+def save_key_point_mat(raw_data, file_name):
+    d2_train_data = k_means_test.get_features(raw_data, whole_image_sample=True, frame_sample=False, global_color=False,
+                                              composition=False, segment_color=False, sift=True)
+    print('kep point mat size: ', d2_train_data.shape)
+    sio.savemat(file_name,
+                mdict={'feature_matrix': d2_train_data,
+                       'label': train_label,
+                       'file_name': train_file_paths,
+                       'index': train_indexes})
+
+
+def save_all_feature_mat(raw_data, file_name):
+    d2_train_data = k_means_test.get_features(raw_data, whole_image_sample=True, frame_sample=False, global_color=True,
+                                              composition=True, segment_color=True, sift=True)
+    print('all feature mat size: ', d2_train_data.shape)
+    sio.savemat(file_name,
+                mdict={'feature_matrix': d2_train_data,
+                       'label': train_label,
+                       'file_name': train_file_paths,
+                       'index': train_indexes})
+
+
+if __name__ == '__main__':
+    w = 250
+    h = w
+    c = 3
+    train_image_count = 10000
+    train_path = r'D:\Projects\pompeii\20190319\svd_test/'
+    train_data, train_label, train_file_paths, train_indexes = read_img_random(train_path, train_image_count,
+                                                                               as_gray=False)
+    np.seterr(all='ignore')
+
+    #
+    save_raw_mat(train_data, 'mat/raw_' + str(w) + '.mat')
+    save_shape_index_mat(train_data, size=10, file_name='mat/shape_index_' + str(10) + '.mat')
+    save_key_point_mat(train_data, file_name='mat/key_point_' + str(12) + '.mat')
+    save_all_feature_mat(train_data, file_name='mat/all_feature.mat')
