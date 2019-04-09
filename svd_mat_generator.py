d48bb659ed1c66b9d2ec8912433c4c219e3831ac
Yingnan Ju <bunny_gg@hotmail.com>
2019-04-09 00:51:46 -0400

    added more functions for svd mat generator

--- svd_mat_generator.py
+++ svd_mat_generator.py
@@ -1,13 +1,14 @@
-from skimage import io, transform
-import numpy as np
 import os
-import random
-import feature_test
+
+import numpy as np
 from scipy import io as sio
+from skimage import io, transform
+
+import feature_test
 import k_means_test
 
 
-def read_img_random(path, total_count, as_gray=False):
+def read_img_random(path, total_count, as_gray=False, resize=None):
     cate = [path + folder for folder in os.listdir(path) if os.path.isdir(path + folder)]
     imgs = []
     labels = []
@@ -26,7 +27,8 @@ def read_img_random(path, total_count, as_gray=False):
             im = file_path_list[count]
             count += 1
             img = io.imread(im, as_gray=as_gray)
-            img = transform.resize(img, (w, h), anti_aliasing=True)
+            if resize is not None:
+                img = transform.resize(img, resize, anti_aliasing=True)
             imgs.append(img)
             labels.append(idx)
             file_names.append(im.split('\\')[-1])
@@ -39,60 +41,71 @@ def read_img_random(path, total_count, as_gray=False):
         indexes, np.int32)
 
 
-def save_raw_mat(raw_data, file_name):
+def save_raw_mat(raw_data, label, file_names, indexes, file_name):
     d2_train_data = k_means_test.get_raw_pixel_features(raw_data)
     print('raw mat size: ', d2_train_data.shape)
     sio.savemat(file_name,
                 mdict={'feature_matrix': d2_train_data,
-                       'label': train_label,
-                       'file_name': train_file_paths,
-                       'index': train_indexes})
+                       'label': label,
+                       'file_name': file_names,
+                       'index': indexes})
 
 
-def save_shape_index_mat(raw_data, size, file_name):
+def save_shape_index_mat(raw_data, label, file_names, indexes, size, file_name):
     d2_train_data = feature_test.get_shape_index_features(raw_data, size=size)
     print('shape index mat size: ', d2_train_data.shape)
     sio.savemat(file_name,
                 mdict={'feature_matrix': d2_train_data,
-                       'label': train_label,
-                       'file_name': train_file_paths,
-                       'index': train_indexes})
+                       'label': label,
+                       'file_name': file_names,
+                       'index': indexes})
 
 
-def save_key_point_mat(raw_data, file_name):
+def save_key_point_mat(raw_data, label, file_names, indexes, file_name):
     d2_train_data = k_means_test.get_features(raw_data, whole_image_sample=True, frame_sample=False, global_color=False,
                                               composition=False, segment_color=False, sift=True)
-    print('kep point mat size: ', d2_train_data.shape)
+    print('key point mat size: ', d2_train_data.shape)
     sio.savemat(file_name,
                 mdict={'feature_matrix': d2_train_data,
-                       'label': train_label,
-                       'file_name': train_file_paths,
-                       'index': train_indexes})
+                       'label': label,
+                       'file_name': file_names,
+                       'index': indexes})
 
 
-def save_all_feature_mat(raw_data, file_name):
+def save_all_feature_mat(raw_data, label, file_names, indexes, file_name):
     d2_train_data = k_means_test.get_features(raw_data, whole_image_sample=True, frame_sample=False, global_color=True,
                                               composition=True, segment_color=True, sift=True)
     print('all feature mat size: ', d2_train_data.shape)
     sio.savemat(file_name,
                 mdict={'feature_matrix': d2_train_data,
-                       'label': train_label,
-                       'file_name': train_file_paths,
-                       'index': train_indexes})
+                       'label': label,
+                       'file_name': file_names,
+                       'index': indexes})
 
 
 if __name__ == '__main__':
-    w = 250
+    w = 500
     h = w
     c = 3
     train_image_count = 10000
-    train_path = r'D:\Projects\pompeii\20190319\svd_test/'
-    train_data, train_label, train_file_paths, train_indexes = read_img_random(train_path, train_image_count,
-                                                                               as_gray=False)
-    np.seterr(all='ignore')
+    train_path = r'D:\Projects\pompeii\20190405\svd_500/'
+    for is_gray in [True, False]:
+        train_data, train_label, train_file_paths, train_indexes = read_img_random(train_path, train_image_count,
+                                                                                   as_gray=is_gray, resize=(h, w))
+        np.seterr(all='ignore')
 
-    #
-    save_raw_mat(train_data, 'mat/raw_' + str(w) + '.mat')
-    save_shape_index_mat(train_data, size=10, file_name='mat/shape_index_' + str(10) + '.mat')
-    save_key_point_mat(train_data, file_name='mat/key_point_' + str(12) + '.mat')
-    save_all_feature_mat(train_data, file_name='mat/all_feature.mat')
+        save_shape_index_mat(train_data, train_label, train_file_paths, train_indexes, size=10,
+                             file_name='mat/shape_index_' + str(10) + ('_g' if is_gray else '') + '.mat')
+        save_key_point_mat(train_data, train_label, train_file_paths, train_indexes,
+                           file_name='mat/key_point_' + str(12) + ('_g' if is_gray else '') + '.mat')
+        if not is_gray:
+            save_all_feature_mat(train_data, train_label, train_file_paths, train_indexes,
+                                 file_name='mat/all_feature.mat')
+
+        for edge in [20, 50, 100, 250]:
+            w = h = edge
+            train_data, train_label, train_file_paths, train_indexes = read_img_random(train_path, train_image_count,
+                                                                                       as_gray=is_gray, resize=(h, w))
+
+            save_raw_mat(train_data, train_label, train_file_paths, train_indexes,
+                         'mat/raw_' + str(w) + ('_g' if is_gray else '') + '.mat')
